94c41b23fd2d8606cdab5120fb8447d14716e1a9
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-10-26 14:36:29 -0300

    Criando componente para reutilizar o Card de produto

---
+++ product-store/src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule} from '@angular/material/card'
+import { MatButtonModule} from '@angular/material/button'
+import { Product } from '../../../../shared/interfaces/product.inteface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  product = input.required<Product>();
+  tituloProduto = computed(() => this.product().title);
+}
